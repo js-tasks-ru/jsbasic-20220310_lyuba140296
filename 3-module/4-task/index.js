d989868d391c2e7f8d79a588b2c4ac3e2d5188f5
Любовь Хуснуллина <lyuba140296@gmail.com>
2022-04-01 11:59:58 -0300

    функция

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
 function showSalary(users, age) {
-  // ваш код...
+  const filterUsers = users.filter(user => user.age <= age);
+  const mappedUsers = filterUsers.map(user => `${user.name}, ${user.balance}`);
+  const result = mappedUsers.join('\n');
+  return result;
 }
