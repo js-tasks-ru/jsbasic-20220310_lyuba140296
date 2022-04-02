ed69e679f369902757509bdc8ff761ce8a3e470e
Любовь Хуснуллина <lyuba140296@gmail.com>
2022-04-01 21:20:40 -0300

    min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(' '); // ['1', 'b', '-5.8']
+  let result = [];
+  for (const num of arr) {
+     let parsed = parseFloat(num);
+     if (!isNaN(parsed)) {
+       result.push(parsed);
+     }
+  }
+  result.sort((a,b) => { return a - b } );
+  console.log(result);
+  
+  return { min: result[0], max: result[result.length - 1]}
 }
